4d2dd24448119d6580747a6f2015bfe8b425693d
中島浩二 <knaka0209@gmail.com>
2023-05-08 20:11:33 +0900

    up,0508a7

--- client/src/pages/TopChatRow.svelte
+++ client/src/pages/TopChatRow.svelte
@@ -4,6 +4,7 @@ import LibConfig from '../lib/LibConfig';
 import Chat from './chats/Chat';
 
 let data: any, chatData: any = {id: 0, name: ""};
+let dislplay_last_chat = false;
 //
 const initPage = async function(): Promise<any>
 {
@@ -13,6 +14,10 @@ const initPage = async function(): Promise<any>
 		if(chatId !== null){
 			chatData = await Chat.get(Number(chatId));
 console.log(chatData);
+			if(chatData.id) {
+				console.log("#dislplay_last_chat");
+				dislplay_last_chat = true;
+			}
 		}         
 //console.log("chatId", chatId);
 	} catch (e) {
@@ -26,6 +31,7 @@ initPage();
 <div>
 	<h3>Recent Chat</h3>
 	<hr />
+	{#if dislplay_last_chat}
 	<a class="nav-link active" aria-current="page" href={`/chats/show/${chatData.id}`}>
 		<span class="task_title fs-5"><h3 class="py-1">{chatData.name}</h3>
 		</span>
@@ -33,4 +39,6 @@ initPage();
 		<button class="btn btn-sm btn-outline-primary mx-2">Open
 		</button>		
 	</a>
+	{/if}
+
 </div>
